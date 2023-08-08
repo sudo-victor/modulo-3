7e83fb141819b46ea0d1e993da13ee576fbdc155
Victor de Souto <victor.souto@hotmail.com>
2023-08-08 15:27:45 -0300

    .

--- 01-nivelamento/example/blog/src/index.js
+++ 01-nivelamento/example/blog/src/index.js
@@ -1,24 +1,41 @@
 import { MakeUser } from "./factories/MakeUser.js"
 
-const logicaUser = MakeUser.getInstance()
+import { MongoClient } from "mongodb"
 
-logicaUser.create({
-  nickname: 'victor',
-  email: 'victor@email.com',
-  password: '123123',
-})
+const client = new MongoClient('mongodb+srv://turma4:arnia4turma@arnia.mjald23.mongodb.net/')
 
-logicaUser.addPost(1, {
-  content: "lorem ipsum"
-})
+const livrosCollection = client.db("Arnia").collection("livros")
 
-logicaUser.addPost(1, {
-  content: "lorem ipsum2"
-})
+;(async () => {
+  await livrosCollection.insertOne({
+    name: "A volta dos que nao foram 2",
+    author: "Lucas Carvalho",
+    versao: 2
+  })
+  const result = await livrosCollection.find().toArray()
+  console.log(result)
+})()
 
-logicaUser.addPost(1, {
-  content: "lorem ipsum3"
-})
 
-console.log(JSON.stringify(logicaUser.findAll()))
+// const logicaUser = MakeUser.getInstance()
+
+// logicaUser.create({
+//   nickname: 'victor',
+//   email: 'victor@email.com',
+//   password: '123123',
+// })
+
+// logicaUser.addPost(1, {
+//   content: "lorem ipsum"
+// })
+
+// logicaUser.addPost(1, {
+//   content: "lorem ipsum2"
+// })
+
+// logicaUser.addPost(1, {
+//   content: "lorem ipsum3"
+// })
+
+// console.log(JSON.stringify(logicaUser.findAll()))
 
